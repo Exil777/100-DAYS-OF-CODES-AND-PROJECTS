79069a04040de43fea6b73e75027d24be8d6d4ca
Jameson <jamesonexil777@gmail.com>
2024-10-16 22:52:41 -0500

    Day One Band Name Generator Project

---
+++ DayOne/Practice.py
@@ -0,0 +1,10 @@
+# String manipilation 
+print("Hello World\nHello World\nHello World")
+
+# string concatination
+print("Hello" + " " +"World")
+
+# find the len of char of value from the user 
+name = input("What is your name?: ")
+nameLength = len(name)
+print(nameLength)
