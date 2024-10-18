e96ce50ca1ec53614813346be0e18ed00d94fcca
Jameson <jamesonexil777@gmail.com>
2024-10-17 21:04:33 -0500

    Day Two Challenge Continue

--- DayTwo/ProjectTwo/TipCalculator.py
+++ DayTwo/ProjectTwo/TipCalculator.py
@@ -2,7 +2,7 @@
 # A calculator that calculate the amount of tips that need to giving base on the user uption and calculate the total amount each person have to pay base of the number the check is being split in bettwen 
 
 print("Welcome to the tip calculator!")
-bill_amount = float(input("What was the bill? "))
+bill_amount = float(input("What was the bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
 bill_splits = int(input("How many people to slip the bill? "))
 
@@ -10,4 +10,4 @@ tips_amount = bill_amount * (tip / 100)
 
 amount_each_person_pay = (bill_amount + tips_amount) / bill_splits
 amount_each_person_pay = round(amount_each_person_pay, 2)
-print(f"Each person should pay: {amount_each_person_pay}")
+print(f"Each person should pay: ${amount_each_person_pay}")
