f97329d9c3701298192471426c095e2157c2da4c
Jameson <jamesonexil777@gmail.com>
2024-10-21 22:47:30 -0500

    Day Four find random friend practice

--- day_four/day_four_practice_2.py
+++ day_four/day_four_practice_2.py
@@ -5,3 +5,9 @@ friends = ["Jake", "Charles", "Jameson", "James", "Peter"]
 
 random_friend = random.choice(friends)
 print(random_friend)
+
+
+# the other other option is to find the random friend is
+random_number = random.randint(0, 4)
+random_index = friends[random_number]
+print(random_index)
