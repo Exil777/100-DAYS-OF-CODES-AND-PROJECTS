fd9266c4a3102833ac4423b8a2c23432ce8d50e6
Jameson <jamesonexil777@gmail.com>
2024-10-23 21:37:36 -0500

    Day five challenge practice continue

--- day_five/day_five_practice.py
+++ day_five/day_five_practice.py
@@ -10,3 +10,25 @@ sum = 0
 for score in student_scores:
     sum += score
 print(sum)
+
+# find the max student scores
+max_score = max(student_scores)
+# finding the max student score with the max function
+print(max_score)
+
+# define a variable and assign the number 0 
+max_student_score = 0
+# use the for loop to go through each score and assign each score the variable score
+for score in student_scores:
+    # checking if max_student_score is less then each student score
+    if max_student_score < score:
+        # assign each student score to max_student_score
+        max_student_score = score
+# and it keep going as long as max_student_score is less than score until it the maximun score 
+print(max_student_score)
+
+# sum up the number from 1 to 100
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
