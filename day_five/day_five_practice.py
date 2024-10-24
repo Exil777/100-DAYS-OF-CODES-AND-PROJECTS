8967e22869203eb3409c3657dfdf0da494a87373
Jameson <jamesonexil777@gmail.com>
2024-10-23 21:00:52 -0500

    Day five challenge practice, finding the total score using loop

---
+++ day_five/day_five_practice.py
@@ -0,0 +1,12 @@
+# find the highest score
+
+student_scores = [150, 140, 125, 199, 160, 70, 90, 85]
+
+# find the sum of the student scores
+total_score = sum(student_scores)
+print(total_score)
+
+sum = 0
+for score in student_scores:
+    sum += score
+print(sum)
