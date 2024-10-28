0ec585cf1ba291b0e8ab3c911b824eb0a51f1e3d
Jameson <jamesonexil777@gmail.com>
2024-10-27 21:05:19 -0500

    Day six Reeborg's world practice with function

---
+++ day_six/day_six_practice.py
@@ -0,0 +1,58 @@
+# creating our own function
+# function are block of codes that we can use over and over
+# # defining our function
+def my_function():
+    print("hello")
+    print("Bye")
+# calling the function
+my_function()
+
+# We use the this website for this week lesson and challenge
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+
+# Hurdle 2 practice with function and while loop
+# this program allow use to mave our robots with bult-in function such as turn_left and I was able to create my own function turn_right, to allow the robot to make a right turn, and I was able to create a function name jump to create a function that allow the robot to jump over the hurdle, because if we only use the turn_left and move() function the amount of code will too much
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump():    
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+
+# while at_goal() != True:
+#     jump()
+
+
+# Hurdle 4 codes for Reeborg's world
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump(): 
+#         turn_left()
+#         while wall_on_right():
+#             move()
+#         turn_right()
+#         move()
+#         turn_right()
+        
+#         while front_is_clear():
+#             move()
+#         turn_left()
+      
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+    
