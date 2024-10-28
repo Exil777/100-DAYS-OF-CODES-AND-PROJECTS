0b2faf917a349592c83e1070c125946512ce027d
Jameson <jamesonexil777@gmail.com>
2024-10-27 21:41:59 -0500

    Day six coding challenge Reeborg's world maze

---
+++ day_six/project_six/reeborg_world_maze.py
@@ -0,0 +1,17 @@
+# this project came from the reeborg's world site and this is the code for the project. 
+# There is a debuging error with this project but I am not in debuging phaze of the 100 days project yet
+# I will come back to it later
+# 
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
