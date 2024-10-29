55be08735d6b5439f370ed313452e192b139fbc3
Jameson <jamesonexil777@gmail.com>
2024-10-28 22:12:54 -0500

    the beginning of my Hangman project

---
+++ day_seven/hangman_project/hangman_project.py
@@ -0,0 +1,7 @@
+word_list = ['apple', 'knife', 'soccer', 'computer']
+
+# TODO-1 Randomly choose a word from the word_list and assign it to a variable called chosen_word, the print it
+
+# TODO-2 Ast the user to guess a letter and assign their answer to a variable called guess. Make guess lower case
+
+# TODO-3 check if the letter the user guessed (guess) is one of the letters in the chosen_word. print "Right" if it is, "Wrong" if it's not
