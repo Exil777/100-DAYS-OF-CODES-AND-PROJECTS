6b0d3f3bbf8b0ab354b7ea6416bb2bcf19cac3f1
Jameson <jamesonexil777@gmail.com>
2024-10-29 21:42:57 -0500

    Hangman project continue

--- day_seven/hangman_project/hangman_project.py
+++ day_seven/hangman_project/hangman_project.py
@@ -7,7 +7,8 @@ print(chosen_word)
 # TODO-2 Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lower case
 guess = input("Guess a letter: ").lower()
 # TODO-3 check if the letter the user guessed (guess) is one of the letters in the chosen_word. print "Right" if it is, "Wrong" if it's not
-if guess in chosen_word:
-    print("Right")
-else:
-    print("Wrong")
+for letter in chosen_word:
+    if letter == chosen_word:
+        print("Right")
+    else:
+        print("Wrong")
