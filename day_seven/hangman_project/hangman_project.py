54d69b0e7013ba4ffd6bda36395a25773f1d3c40
Jameson <jamesonexil777@gmail.com>
2024-10-29 21:38:53 -0500

    First session of hangman project

--- day_seven/hangman_project/hangman_project.py
+++ day_seven/hangman_project/hangman_project.py
@@ -1,7 +1,13 @@
+import random
 word_list = ['apple', 'knife', 'soccer', 'computer']
 
 # TODO-1 Randomly choose a word from the word_list and assign it to a variable called chosen_word, the print it
-
-# TODO-2 Ast the user to guess a letter and assign their answer to a variable called guess. Make guess lower case
-
+chosen_word = random.choice(word_list)
+print(chosen_word)
+# TODO-2 Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lower case
+guess = input("Guess a letter: ").lower()
 # TODO-3 check if the letter the user guessed (guess) is one of the letters in the chosen_word. print "Right" if it is, "Wrong" if it's not
+if guess in chosen_word:
+    print("Right")
+else:
+    print("Wrong")
