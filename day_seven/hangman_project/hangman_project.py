7e8227ad4a583e82e9531956cc1c938499a53e05
Jameson <jamesonexil777@gmail.com>
2024-10-29 22:10:46 -0500

    Fixed a small bug and also completed two more todo's. Created place holders for the random words and replace some of the place holders with letters

--- day_seven/hangman_project/hangman_project.py
+++ day_seven/hangman_project/hangman_project.py
@@ -4,11 +4,25 @@ word_list = ['apple', 'knife', 'soccer', 'computer']
 # TODO-1 Randomly choose a word from the word_list and assign it to a variable called chosen_word, the print it
 chosen_word = random.choice(word_list)
 print(chosen_word)
+# TODO-4 Create a "placeholder" with the same number of blanks as the chosen_word
+placeholder = ""
+word_length = len(chosen_word)
+
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
 # TODO-2 Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lower case
 guess = input("Guess a letter: ").lower()
 # TODO-3 check if the letter the user guessed (guess) is one of the letters in the chosen_word. print "Right" if it is, "Wrong" if it's not
+# TODO-5 Create a "display" that puts the guess letter in the right position and _ in the rest of the string.
+display = ""
+
 for letter in chosen_word:
-    if letter == chosen_word:
-        print("Right")
+    if letter == guess:
+        #print("Right")
+        display += letter
     else:
-        print("Wrong")
+        #print("Wrong")
+        display += "_"
+
+print(display)
