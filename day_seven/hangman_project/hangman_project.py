a4a070a480432f8f81bfd6bd0370fae22f35fe47
Jameson <jamesonexil777@gmail.com>
2024-10-30 21:39:18 -0500

    Todo 6 and Todo 7 of our hangman game

--- day_seven/hangman_project/hangman_project.py
+++ day_seven/hangman_project/hangman_project.py
@@ -11,18 +11,30 @@ word_length = len(chosen_word)
 for position in range(word_length):
     placeholder += "_"
 print(placeholder)
+
+# TODO-6 Use a while loop to let the user guess again
+game_over = False
+correct_letters = []
 # TODO-2 Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lower case
-guess = input("Guess a letter: ").lower()
+while not game_over:
+    guess = input("Guess a letter: ").lower()
 # TODO-3 check if the letter the user guessed (guess) is one of the letters in the chosen_word. print "Right" if it is, "Wrong" if it's not
 # TODO-5 Create a "display" that puts the guess letter in the right position and _ in the rest of the string.
-display = ""
+    display = ""
+#TODO-7 Change the for loop so that you keep the previous correct letters in the diplay
+    for letter in chosen_word:
+        if letter == guess:
+            #print("Right")
+            display += letter
+            correct_letters.append(letter)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            #print("Wrong")
+            display += "_"
 
-for letter in chosen_word:
-    if letter == guess:
-        #print("Right")
-        display += letter
-    else:
-        #print("Wrong")
-        display += "_"
+    print(display)
 
-print(display)
+    if "_" not in display:
+        game_over = True
+        print("You win.")
