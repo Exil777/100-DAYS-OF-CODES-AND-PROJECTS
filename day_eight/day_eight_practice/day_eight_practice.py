5f3e02203e815b0721fa0aae7c3eae1b2d8f9bb8
Jameson <jamesonexil777@gmail.com>
2024-11-05 17:06:59 -0600

    Day eight function and parameters and arguments

---
+++ day_eight/day_eight_practice/day_eight_practice.py
@@ -0,0 +1,12 @@
+# Function and parameters
+def greet():
+    print("Hello World")
+    print("My name is Jameson")
+
+greet()
+# declaring a function call 'great()' and calling the function
+
+def greet_with_name(first_name, last_name):
+    print(f"Hello my name is {first_name} {last_name}")
+greet_with_name("Jameson", "Exil")
+# declaring a function call 'greet_with_name' with two parameters and passing two arguments when calling the function
