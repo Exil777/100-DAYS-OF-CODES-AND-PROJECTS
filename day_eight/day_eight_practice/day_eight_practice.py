0de65061cbc85ac2e41460cce301c666ba6dee39
Jameson <jamesonexil777@gmail.com>
2024-11-05 17:27:10 -0600

    Weeks left until 90 years old from current age practice

--- day_eight/day_eight_practice/day_eight_practice.py
+++ day_eight/day_eight_practice/day_eight_practice.py
@@ -10,3 +10,17 @@ def greet_with_name(first_name, last_name):
     print(f"Hello my name is {first_name} {last_name}")
 greet_with_name("Jameson", "Exil")
 # declaring a function call 'greet_with_name' with two parameters and passing two arguments when calling the function
+
+#Create a function called life_in_weeks() using maths and f-Strings that tells us how many weeks we have left, if we live until 90 years old.
+def life_in_weeks(current_age):
+
+    total_years = 90
+    weeks_in_year = 52
+    
+    weeks_in_total_year = total_years * weeks_in_year
+    weeks_in_current_age = current_age * weeks_in_year 
+
+    years_left = weeks_in_total_year - weeks_in_current_age
+    print(f"You have {years_left} weeks left.")
+
+life_in_weeks(56)
