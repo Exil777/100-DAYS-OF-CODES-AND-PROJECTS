ca41dc49b198832d07d1b7bff1f5887a7e39d5e8
Jameson <jamesonexil777@gmail.com>
2024-11-06 19:10:26 -0600

    final practice

--- day_eight/day_eight_practice/day_eight_practice.py
+++ day_eight/day_eight_practice/day_eight_practice.py
@@ -31,3 +31,33 @@ def greet_with(name, location):
     print(f"What is it like in {location}")
 greet_with(name="Jameson", location="Dallas")
 # declaring 'greet_with' function with two parameter and two keyword arguments
+
+
+
+#You are going to write a function called calculate_love_score() that tests the compatibility between two names.  To work out the love score between two people: 
+
+#1. Take both people's names and check for the number of times the letters in the word TRUE occurs.   
+
+#2. Then check for the number of times the letters in the word LOVE occurs.   
+
+#3. Then combine these numbers to make a 2 digit number and print it out. 
+def calculate_love_score(name1, name2):
+    names_combine = name1 + name2
+    lower_names = names_combine.lower()
+
+    t = lower_names.count("t")
+    r = lower_names.count("r")
+    u = lower_names.count("u")
+    e = lower_names.count("e")
+    first_compatibility = t + r + u + e 
+
+    l = lower_names.count("l")
+    o = lower_names.count("o")
+    v = lower_names.count("v")
+    e = lower_names.count("e")
+    sencond_compatibility = l + o + v + e 
+
+    total_number_compatibility = int(str(first_compatibility) + str(sencond_compatibility))
+    print(total_number_compatibility)
+
+calculate_love_score("Jameson", "Exil")
