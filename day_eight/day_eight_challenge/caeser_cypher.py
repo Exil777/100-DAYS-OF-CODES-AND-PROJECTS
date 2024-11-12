d2be38d1d9ef41115d4e9460f83d8d9593660e8b
Jameson <jamesonexil777@gmail.com>
2024-11-11 22:03:03 -0600

    Combining two function into one function so i can the encoded or the decoded text

--- day_eight/day_eight_challenge/caeser_cypher.py
+++ day_eight/day_eight_challenge/caeser_cypher.py
@@ -6,40 +6,53 @@ shift = int(input("Type the shift number:\n"))
 
 
 # TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
-def encrupt(original_text, shift_amount):
+# def encrupt(original_text, shift_amount):
 # TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
 #  by the shift amount and print the encrypted text.
-    cipher_text = ""
-    for letter in original_text:
-        shifted_position = alphabet.index(letter) + shift_amount
+    # cipher_text = ""
+    # for letter in original_text:
+    #     shifted_position = alphabet.index(letter) + shift_amount
 
-        shifted_position %= len(alphabet) 
-        cipher_text += alphabet[shifted_position]
-    print(f"Encoded result {cipher_text}")
+    #     shifted_position %= len(alphabet) 
+    #     cipher_text += alphabet[shifted_position]
+    # print(f"Encoded result {cipher_text}")
 # TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
 
 # TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
 #  message.
-encrupt(original_text=text, shift_amount=shift)
+# encrupt(original_text=text, shift_amount=shift)
 
 # TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
 # TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
 #  by the shift amount and print the decrypted text.
+
+# def decrypt(original_text, shift_amount):
+#     output_text = ""
+#     for letter in original_text:
+#         shifted_position = alphabet.index(letter) - shift_amount
+
+#         shifted_position %= len(alphabet)
+#         output_text += alphabet[shifted_position]
+
+#     print(f"Decoded result {output_text}")
+
+# decrypt(original_text=text, shift_amount=shift)
 # TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
 #  Use the value of the user chosen 'direction' variable to determine which functionality to use.
+def caesar(original_text, shift_amount, decode_or_encode):
 
-def decrypt(original_text, shift_amount):
     output_text = ""
+    if decode_or_encode == "decode":
+            shift_amount *= -1
     for letter in original_text:
-        shifted_position = alphabet.index(letter) - shift_amount
-
+        
+        shifted_position = alphabet.index(letter) + shift_amount
         shifted_position %= len(alphabet)
         output_text += alphabet[shifted_position]
 
-    print(f"Decoded result {output_text}")
+    print(f"Here is the {decode_or_encode}d result: {output_text}")
 
-decrypt(original_text=text, shift_amount=shift)
-
+caesar(original_text=text, shift_amount=shift, decode_or_encode=direction)
 
 
 
