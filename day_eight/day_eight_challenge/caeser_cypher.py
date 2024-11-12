154ca51f4fd8675526baf56dac42c4cd37fbbe55
Jameson <jamesonexil777@gmail.com>
2024-11-11 22:19:38 -0600

    Final caeser project

--- day_eight/day_eight_challenge/caeser_cypher.py
+++ day_eight/day_eight_challenge/caeser_cypher.py
@@ -1,8 +1,42 @@
+from art import logo
+print(logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def caesar(original_text, shift_amount, decode_or_encode):
+
+    output_text = ""
+    if decode_or_encode == "decode":
+            shift_amount *= -1
+    for letter in original_text:
+        if letter not in alphabet:
+             output_text += letter
+        else:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+
+    print(f"Here is the {decode_or_encode}d result: {output_text}")
+
+
+game_continue = True
+while game_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, decode_or_encode=direction)
+
+    resart = input("Type 'yes' if you want to continue. Otherwise type 'no'\n").lower()
+    if resart == "no":
+         game_continue = False
+         print("Thank you for playing. Good bye.")
+
+
+# TODO-1: Import and print the logo from art.py when the program starts.
+# TODO-2: What happens if the user enters a number/symbol/space?
+# TODO-3: Can you figure out a way to restart the cipher program?
+
+
 
 
 # TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
@@ -39,24 +73,3 @@ shift = int(input("Type the shift number:\n"))
 # decrypt(original_text=text, shift_amount=shift)
 # TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
 #  Use the value of the user chosen 'direction' variable to determine which functionality to use.
-def caesar(original_text, shift_amount, decode_or_encode):
-
-    output_text = ""
-    if decode_or_encode == "decode":
-            shift_amount *= -1
-    for letter in original_text:
-        
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        output_text += alphabet[shifted_position]
-
-    print(f"Here is the {decode_or_encode}d result: {output_text}")
-
-caesar(original_text=text, shift_amount=shift, decode_or_encode=direction)
-
-
-
-
-# TODO-1: Import and print the logo from art.py when the program starts.
-# TODO-2: What happens if the user enters a number/symbol/space?
-# TODO-3: Can you figure out a way to restart the cipher program?
