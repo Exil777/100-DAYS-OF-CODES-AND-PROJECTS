749d938e4a1d7f6f4a6e87f344a39010bc1b4116
Jameson <jamesonexil777@gmail.com>
2024-11-15 21:32:24 -0600

    Todo 1 and Todo 2 day nice project

--- day_nine/day_nine_project/silent_auction.py
+++ day_nine/day_nine_project/silent_auction.py
@@ -1,4 +1,9 @@
 # TODO-1: Ask the user for input
+print("Welcome to the Silent Auction game.")
+name = input("What is your name? ")
+price = int(input("What's your price? "))
 # TODO-2: Save data into dictionary {name: price}
+bids = {}
+bids[name] = price
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
