a1fdfe104221dc1552ac61b8270bad3946825e1b
Jameson <jamesonexil777@gmail.com>
2024-11-15 21:48:42 -0600

    Silent auction Todo 3, creating a solution to where other player can add bids

--- day_nine/day_nine_project/silent_auction.py
+++ day_nine/day_nine_project/silent_auction.py
@@ -1,9 +1,18 @@
 # TODO-1: Ask the user for input
 print("Welcome to the Silent Auction game.")
-name = input("What is your name? ")
-price = int(input("What's your price? "))
+# name = input("What is your name? ")
+# price = int(input("What's your price? "))
 # TODO-2: Save data into dictionary {name: price}
 bids = {}
-bids[name] = price
+# bids[name] = price
 # TODO-3: Whether if new bids need to be added
+continue_bids = True
+while continue_bids:
+    name = input("What is your name? ")
+    price = int(input("What's your price? "))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
+
+    if should_continue == "no":
+        continue_bids = False
 # TODO-4: Compare bids in dictionary
