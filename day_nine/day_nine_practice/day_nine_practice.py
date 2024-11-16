6ad4a56bcefee8d2675c4c9c610fcbafb81776f8
Jameson <jamesonexil777@gmail.com>
2024-11-15 21:28:58 -0600

    day nine practice with dictionary

--- day_nine/day_nine_practice/day_nine_practice.py
+++ day_nine/day_nine_practice/day_nine_practice.py
@@ -17,3 +17,36 @@ for key in programing_dictionary:
     print(programing_dictionary[key])
 
 
+
+# dictionary
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+# Nested List in Dict
+# travel_log = {
+#     "France": ["Paris", "Lille", "Dihon"],
+#     "Germany": ["stuttgart", "Berlin"],
+# }
+
+# print(travel_log["France"][1])
+
+# Nested list
+nested_list = ["A", "B", ["C", "D"]]
+print(nested_list[2][1])
+
+# Nested Dict in Dict
+travel_log = {
+    "France": {
+        "total_visits": 12,
+        "cities_visited": ["Paris", "lille", "Dijon"]
+    },
+    "Germany": {
+        "total_visits": 8,
+        "cities_visited": ["stuttgart", "Berlin"],
+    }
+}
+
+print(travel_log["France"]["total_visits"])
+print(travel_log["Germany"]["cities_visited"][1])
