16f0210a81efd23e5ce2a82d8af1f8ed10cd4eff
Jameson <jamesonexil777@gmail.com>
2024-11-17 15:49:00 -0600

    Function with multiple return value

--- day_ten/day_ten_practice/day_ten_practice.py
+++ day_ten/day_ten_practice/day_ten_practice.py
@@ -1,8 +1,10 @@
 # Functiion with output
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You did not provide a valid inputs"
     first_name = f_name.title()
     last_name = l_name.title()
     return f"{first_name} {last_name}"
 
-formatd_string = format_name(f_name="jameson", l_name="exil")
+formatd_string = format_name(input("What is your first name? "), input("What is your last name? "))
 print(formatd_string)
