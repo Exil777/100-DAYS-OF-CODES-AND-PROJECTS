9cbd1578f39d950d8dc65ca121dd6623b8caf8d7
Jameson <jamesonexil777@gmail.com>
2024-11-17 19:38:50 -0600

    Leap year solution practice

--- day_ten/day_ten_practice/day_ten_practice.py
+++ day_ten/day_ten_practice/day_ten_practice.py
@@ -8,3 +8,30 @@ def format_name(f_name, l_name):
 
 formatd_string = format_name(input("What is your first name? "), input("What is your last name? "))
 print(formatd_string)
+
+
+# Write a program that returns True or False whether if a given year is a leap year.
+
+# This is how you work out whether if a particular year is a leap year. 
+
+# - on every year that is divisible by 4 with no remainder
+
+# - except every year that is evenly divisible by 100 with no remainder 
+
+# - unless the year is also divisible by 400 with no remainder   
+
+
+def is_leap_year(year):
+    # Write your code here. 
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+    # Don't change the function name.
+print(is_leap_year(2400))
